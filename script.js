09523c991a51ca70840c6f4f9fb9bc4e8803d6a3
Simone-Bruno98 <bruno-simone@outlook.it>
2021-12-12 15:41:11 +0100

    Milestone 1 completato

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 //let i = 1;
 
 for (let i = 1; i <= 100; ++i) {
-    console.log ("Numeri: ", i);
+        if ((i%3 == 0) && (i%5 == 0)) {
+            console.log ("fizzbuzz");
+        } else if (i%5 == 0) {
+            console.log ("buzz");
+        } else if (i%3 == 0){
+            console.log ("fizz");
+        } else {
+            console.log (i)
+        }
+    //console.log (i);
+    
 }
 
-
-//document.writeln(i);
